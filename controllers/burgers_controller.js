f8facfb0656cc9999711be5d2b80d975bd8d0f3d
drclever <drc0625@aol.com>
2021-01-04 23:52:23 -0500

    Initial load of routes logic

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+const burger = require("../models/burger");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", (req, res) => {
+    burger.selectAll(data => {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burger", (req, res) => {
+    burger.insertOne("burger_name", req.body.burger_name, result => {
+        // Send back the ID of the new burger
+        res.json({id: result.insertId});
+    });
+});
+
+router.put("/api/burger/:id", (req, res) => {
+    var burgerId = req.params.id;
+  
+    burger.updateOne(burgerId, result => {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+// Export routes for server.js to use.
+module.exports = router;
