45c6ad44e55570d3bc5b5a97f9071f7274d1c5e1
drclever <drc0625@aol.com>
2021-01-04 23:44:57 -0500

    Initial Load of schema and seed files for MySQL

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Hamburger");
+INSERT INTO burgers (burger_name)
+VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie Burger");
