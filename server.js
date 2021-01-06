7472a8322770407eaa66351c50f8044328b3968e
drclever <drc0625@aol.com>
2021-01-05 22:11:04 -0500

    Changing logic to use ES6

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Dependencies
-var express = require('express');
+const express = require('express');
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -14,13 +14,13 @@ app.use(express.json());
 app.use("/public", express.static("./public"));
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller");
+const routes = require("./controllers/burgers_controller");
 
 app.use(routes);
 
